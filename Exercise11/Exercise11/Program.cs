f03d2c23527fe916712a7aa5f8541f1db770317c
dxmendoza <damian.x.mendoza@gmail.com>
2025-07-18 11:05:44 -0400

    Add files via upload

---
+++ Exercise11/Exercise11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ConsoleCards;
+
+namespace Exercise11
+{
+    /// <summary>
+    /// Exercise 11
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Exercise 11
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            Card[] hand = new Card[5];
+            deck.Shuffle();
+            hand[0] = deck.TakeTopCard();
+            hand[0].FlipOver();
+            hand[0].Print();
+        }
+    }
+}
---
+++ ProgrammingAssignmentCards/ProgrammingAssignment3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using ConsoleCards;
+
+namespace ProgrammingAssignment3
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 3
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Programming Assignment 3
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // loop while there's more input
+            string input = Console.ReadLine();
+            while (input[0] != 'q')
+            {
+                // Add your code between this comment
+                // and the comment below. You can of
+                // course add more space between the
+                // comments as needed
+
+                // declare a deck variables and create a deck object
+                // DON'T SHUFFLE THE DECK
+                Deck deck = new Deck();
+
+                // deal 2 cards each to 4 players (deal properly, dealing
+                // the first card to each player before dealing the
+                // second card to each player)
+                Card card1 = deck.TakeTopCard();
+                Card card2 = deck.TakeTopCard();
+                Card card3 = deck.TakeTopCard();
+                Card card4 = deck.TakeTopCard();
+                Card card5 = deck.TakeTopCard();
+                Card card6 = deck.TakeTopCard();
+                Card card7 = deck.TakeTopCard();
+                Card card8 = deck.TakeTopCard();
+
+                // deal 1 more card to players 2 and 3
+                Card card9 = deck.TakeTopCard();
+                Card card10 = deck.TakeTopCard();
+
+                // flip all the cards over
+                card1.FlipOver();
+                card2.FlipOver();
+                card3.FlipOver();
+                card4.FlipOver();
+                card5.FlipOver();
+                card6.FlipOver();
+                card7.FlipOver();
+                card8.FlipOver();
+                card9.FlipOver();
+                card10.FlipOver();
+
+                // print the cards for player 1
+                Console.WriteLine(card1.Rank + "," + card1.Suit);
+                Console.WriteLine(card5.Rank + "," + card5.Suit);
+
+                // print the cards for player 2
+                Console.WriteLine(card2.Rank + "," + card2.Suit);
+                Console.WriteLine(card6.Rank + "," + card6.Suit);
+                Console.WriteLine(card9.Rank + "," + card9.Suit);
+
+                // print the cards for player 3
+                Console.WriteLine(card3.Rank + "," + card3.Suit);
+                Console.WriteLine(card7.Rank + "," + card7.Suit);
+                Console.WriteLine(card10.Rank + "," + card10.Suit);
+
+                // print the cards for player 4
+                Console.WriteLine(card4.Rank + "," + card4.Suit);
+                Console.WriteLine(card8.Rank + "," + card8.Suit);
+
+                // Don't add or modify any code below
+                // this comment
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
